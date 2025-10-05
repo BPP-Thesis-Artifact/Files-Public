fef0d1510450789b1758c288016d6eefddc2bf64
LPfahl-UL <leonpfahl2002@gmail.com>
2025-10-05 16:35:02 +0200

    Removed previous push

--- sources/AsteriskAndAmpersand.cpp
+++
@@ -1,9 +0,0 @@
-// AsteriskAndAmpersand.cpp by Ulrich Eisenecker, April 20 2021
-
-int main()
-{
-   int * pointerToInt { nullptr }, anInt { 42 };
-   
-   double d { 99.0 };
-   double & referenceToDouble { d }, aDouble { 100.0 };
-}
--- sources/RatNumRefSem_1/math_helper.hpp
+++
@@ -1,3 +0,0 @@
-#include <cstdint> // Because of intmax_t
-
-[[nodiscard]] std::intmax_t sign(const std::intmax_t& n);
